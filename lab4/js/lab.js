389fd27e5fcb66c1963a5963ed62f26f77e4b0c8
armanjmoslehi <ajmosleh@ucsc.edu>
2024-10-16 17:25:01 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,30 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Arman Moslehi
+// Date: 10/15/2024
 
-// Constants
+// Rock Paper Scissors:
 
-// Functions
+// Give user three options to choose from Rock, Paper, Or Scissors
+    // Print image of rock, image of paper, and image of scissors from left to right
+// Accept user’s input
+// Random generate computer response from Rock, Paper or Scissors
+// Reveal computer’s choice to user
+    // Move User’s choice to left
+    // Print computers choice next to User’s on the right 
+//Determine win, loss, or draw
+    // Win condition, player chooses Rock if computer chooses Scissors
+    // Win condition, player chooses Paper if computer chooses Rock
+    // Win condition, player chooses Scissors if computer chooses Paper
+    // Lose condition, player chooses Rock if computer chooses Paper
+    // Lose condition, player chooses Paper if computer chooses Scissors
+    // Lose condition, player chooses Scissors if computer chooses Rock
+    // Draw Condition, player and computer have chosen the same option
+// If draw, repeat from step one
+// If win or loss, display appropriate tag according to win or loss
+  // Print “You win!” if user wins
+  // Print “You lose!” if user loses
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
 
-// let's get this party started
+
 main();
