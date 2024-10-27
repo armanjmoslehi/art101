29304c6950aec1dff25b7532deae1bd2e77da8a8
armanjmoslehi <ajmosleh@ucsc.edu>
2024-10-26 18:10:35 -0700

    Update lab.js

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -30,23 +30,36 @@ function sortUserName(userName) {
     return userName.toLowerCase().split("").sort().join("");
 }
 
-
 function sortUserName(){
-    var userName = window.prompt("Hi! Type in your name so it can be improved.");
-    console.log("username =", userName);
+    var userName = window.prompt("Hey! Please let me improve your name by typing it in here:");
+    console.log("userName=", userName);
 
     var nameArray = userName.split('');
-    console.log("nameArray =", nameArray);
+    console.log("nameArray=", nameArray);
 
     var nameArraySort = nameArray.sort();
     console.log("nameArraySort =", nameArraySort);
 
-    var nameSorted = nameArraySort.join('');
-    console.log("nameSorted =", nameSorted);
+    var nameSorted = nameArraySort.join('')
+    console.log("nameSorted=", nameSorted);
 
     return nameSorted;
-
 }
+
+function randomizeName(userName) {
+    
+    var nameArray = userName.toLowerCase().split("");
+    console.log("nameArray =", nameArray);
+    
+    var shuffledArray = shuffleArray(nameArray);
+    console.log("shuffledArray =", shuffledArray);
+    var shuffledString = shuffledArray.join("");
+    
+    var newName = toTitleCase(shuffledString);
+    
+    return newName;
+}
+
 
     document.writeln("Wow!! Your name is SO much better now: ",
         sortUserName(), "</br>");
